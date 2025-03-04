5690e42687f53b9317ce991148f15e330513aec7
Olivia Boone <144468291+Olivia9444@users.noreply.github.com>
2025-03-04 12:28:57 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var name = prompt("what is your name?");
+var birthYear = prompt("what year were you born? (ex. 1995)");
+
+console.log("Your name is ${name} and you're ${2025 - birthYear} years old.");
